4b1c66cecb7c3581d78b7ba524fd64d68f87a9dc
Mark Wutka <mark.wutka@vanderbilt.edu>
2021-04-29 11:44:29 -0500

    Added output directory option

--- gen_project.py
+++ gen_project.py
@@ -37,14 +37,21 @@ def write_elementtree(elem, filename):
 system_lib_path = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "lib")
 
 arg_parser = argparse.ArgumentParser(description="Generate UxAS Configuration")
-arg_parser.add_argument('-libpath', nargs=2, default="")
+arg_parser.add_argument('-libpath', nargs=1, default="")
+arg_parser.add_argument('-o', nargs=1, default=os.curdir)
 
 parsed_args, rest_args = arg_parser.parse_known_args()
 parsed_args = vars(parsed_args)
 
+output_dir = parsed_args["o"][0]
+print("output_dir=",output_dir)
+
 lib_path_args = []
 if "libpath" in parsed_args:
-    lib_path_args = parsed_args["libpath"].split(os.pathsep)
+    if isinstance(parsed_args["libpath"], list):
+        lib_path_args = parsed_args["libpath"][0].split(os.pathsep)
+    else:
+        lib_path_args = parsed_args["libpath"].split(os.pathsep)
 
 lib_path = lib_path_args + [system_lib_path]
 
@@ -65,7 +72,7 @@ amase_schema_parser = load_amase_schemas(lib_path)
 amase_renderer = UxasXMLRenderer(amase_schema_parser.schemas)
 
 messages = []
-os.makedirs("MessagesToSend/tasks", 0o777, exist_ok=True)
+os.makedirs(os.path.join(output_dir, "MessagesToSend", "tasks"), 0o777, exist_ok=True)
 
 scen_veh_list = []
 scen_veh_state_list = []
@@ -79,14 +86,14 @@ for vehicle_id in uxas_plan_parser.configs[0]["Entities"]:
         tree = ET.ElementTree(vehicle_xml)
         filename = "AirVehicleConfiguration_V"+str(vehicle_id)+".xml"
         messages.append({"MessageFileName": filename, "SendTime_ms": 200})
-        write_elementtree(vehicle_xml, "MessagesToSend/"+filename)
+        write_elementtree(vehicle_xml, os.path.join(output_dir, "MessagesToSend", filename))
         scen_veh_list.append(vehicle)
 
         vehicle_state_xml = renderer.render(None, vehicle["state"])
         tree = ET.ElementTree(vehicle_state_xml)
         filename = "AirVehicleState_V"+str(vehicle_id)+".xml"
         messages.append({"MessageFileName": filename, "SendTime_ms": 250})
-        write_elementtree(vehicle_state_xml, "MessagesToSend/"+filename)
+        write_elementtree(vehicle_state_xml, os.path.join(output_dir, "MessagesToSend", filename))
         scen_veh_state_list.append(vehicle["state"])
 
 amase_config = {
@@ -101,14 +108,14 @@ for task in uxas_plan_parser.configs[0]["Tasks"]:
     tree = ET.ElementTree(task_xml)
     filename = "tasks/"+str(task["TaskID"])+"_"+task["Label"]+".xml"
     messages.append({"MessageFileName": filename, "SendTime_ms": 300})
-    write_elementtree(task_xml, "MessagesToSend/"+filename)
+    write_elementtree(task_xml, os.path.join(output_dir, "MessagesToSend", filename))
 
 for sched in uxas_plan_parser.configs[0]["Schedule"]:
     sched_xml = renderer.render(None, sched)
     tree = ET.ElementTree(sched_xml)
     filename = "tasks/"+str(sched["ID"])+"_AutomationRequest_"+sched["Label"]+".xml"
     messages.append({"MessageFileName": filename, "SendTime_ms": sched["StartDelay"]})
-    write_elementtree(sched_xml, "MessagesToSend/"+filename)
+    write_elementtree(sched_xml, os.path.join(output_dir, "MessagesToSend", filename))
 
 sendMessageService = {"struct_type": "service", "type": "SendMessagesService",
                        "PathToMessageFiles": "../MessagesToSend/",
@@ -119,13 +126,13 @@ uxas_parser.configs[0]["services"].append(sendMessageService)
 config = renderer.render(None, uxas_parser.configs[0])
 
 config_filename = "cfg_"+uxas_parser.configs[0]["Name"]+".xml"
-write_elementtree(config, config_filename)
+write_elementtree(config, os.path.join(output_dir, config_filename))
 
 amase_xml = amase_renderer.render(None, amase_config)
 scenario_filename = "Scenario_"+uxas_parser.configs[0]["Name"]+".xml"
-write_elementtree(amase_xml, scenario_filename)
+write_elementtree(amase_xml, os.path.join(output_dir, scenario_filename))
 
-config_file = open("config.yaml", "w")
+config_file = open(os.path.join(output_dir, "config.yaml"), "w")
 config_file.writelines(["amase:\n",
                         "  scenario: "+scenario_filename+"\n",
                         "\n",
