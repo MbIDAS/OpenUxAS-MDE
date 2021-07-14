cb9a74fa6a617239afd77b4ec27d54312efcdd77
Mark Wutka <mark.wutka@vanderbilt.edu>
2021-07-14 16:56:57 -0500

    Added LMCP generation

---
+++ src/openuxas_mde/lmcp/parser.py
@@ -0,0 +1,106 @@
+from textx.metamodel import metamodel_from_file
+import textx.model
+import os.path
+import sys
+import pkg_resources
+from openuxas_mde.lang.util import get_filename_in_path
+
+
+class ParseError(Exception):
+    def __init__(self, error, obj=None, parser=None):
+        self.error = error
+        self.line = -1
+        self.col = -1
+        if obj is not None and parser is not None:
+            line, col = parser.pos_to_linecol(obj._tx_position)
+            self.line = line
+            self.col = col
+            self.error = "Line {}, Col {}: {}".format(line, col, self.error)
+
+
+lang_dir = pkg_resources.resource_filename("openuxas_mde", "lmcp")
+lmcp_meta = metamodel_from_file(os.path.join(lang_dir, "../lib/lmcp.tx"))
+
+class LMCPParser:
+    def __init__(self, lib_path):
+        self.configs = {}
+        self.lib_dir = lib_path
+
+    def simplify_ast(self, node):
+        if textx.textx_isinstance(node, lmcp_meta.namespaces["lmcp"]["lmcpConfig"]):
+            simplified_enums = []
+            for e in node.enums.enumList:
+                simplified_enums.append(self.simplify_ast(e))
+
+            simplified_structs = []
+            for s in node.structs.structList:
+                simplified_structs.append(self.simplify_ast(s))
+
+            return {"struct_type": "mdm", "type": "", "seriesName": node.seriesName, "namespace": node.namespace,
+                    "version": node.version, "enums": simplified_enums,
+                    "structs": simplified_structs}
+        elif textx.textx_isinstance(node, lmcp_meta.namespaces["lmcp"]["lmcpEnum"]):
+            value_list = []
+            for v in node.values:
+                value_list.append({"struct_type": "Entry", "type": "", "name": v.enumName, "value": v.enumValue.value})
+            return {"struct_type": "Enum", "type": "", "name": node.name, "values": value_list}
+        elif textx.textx_isinstance(node, lmcp_meta.namespaces["lmcp"]["lmcpStruct"]):
+            extension = None
+            if node.extension is not None:
+                extension = node.extension.reference
+            fields = []
+            for f in node.fields:
+                fields.append(self.simplify_ast(f))
+            return {"struct_type": "Struct", "type": "", "name": node.name, "extends": extension, "fields": fields}
+        elif textx.textx_isinstance(node, lmcp_meta.namespaces["lmcp"]["lmcpStructField"]):
+            default = None
+            if node.default is not None:
+                default = node.default.value.value
+            units = None
+            if node.units is not None:
+                units = node.units.value
+            size = None
+            if node.size is not None:
+                size = node.size.size
+            return {"struct_type": "Field", "type": "", "name": node.name, "type": node.type, "size": size,
+                    "default": default, "units": units}
+
+    def simplify(self, config):
+        return self.simplify_ast(config)
+
+    def load_config_from_file(self, filename):
+        try:
+            config = lmcp_meta.model_from_file(filename)
+            return self.simplify(config)
+        except textx.exceptions.TextXSyntaxError as exc:
+            self.diagnose_syntax_error(exc, filename)
+
+    def diagnose_syntax_error(self, exc, filename):
+        f = open(filename, 'r')
+        curr_line = 0
+        for line in f:
+            curr_line += 1
+            if curr_line > exc.line:
+                break
+            if self.count_quotes(line) % 2 == 1:
+                print(exc.message)
+                print("Possible unclosed quote in file "+filename+" at line "+str(curr_line))
+                print(line)
+                raise ParseError(exc)
+        print(exc.message)
+        raise ParseError(exc)
+
+
+    def count_quotes(self, line):
+        pos = 0
+        quote_count = 0
+        while pos < len(line):
+            if line[pos] == '"':
+                if pos < len(line)-1:
+                    if line[pos+1] != '"':
+                        quote_count += 1
+                    else:
+                        pos += 2
+                        continue
+            pos += 1
+        return quote_count
---
+++ src/openuxas_mde/lmcp/render.py
@@ -0,0 +1,189 @@
+import xml.etree.ElementTree as ET
+
+class Environment:
+    def __init__(self, dict, next_env=None):
+        self.dict = dict
+        self.next = next_env
+
+    def lookup(self, key):
+        if key in self.dict:
+            return self.dict[key]
+        elif self.next is not None:
+            return self.next.lookup(key)
+        else:
+            return None
+
+
+class RenderError(Exception):
+    def __init__(self, error):
+        self.error = error
+
+class LMCPXMLRenderer:
+    def __init__(self, schemas):
+        self.schemas = schemas
+
+    def get_value(self, obj, value, env, foreach_map):
+        if value["value_type"] == "string":
+            return value["value"]
+        elif value["value_type"] == "named":
+            if value["name"] not in obj:
+                raise RenderError("For value "+value["name"]+" no such value in object")
+            obj_value = obj[value["name"]]
+            return obj_value
+        elif value["value_type"] == "current":
+            return obj
+        elif value["value_type"] == "foreach":
+            if value["foreach"] not in foreach_map:
+                raise RenderError("For value "+value["foreach"]+"."+value["name"]+
+                                  ", parent structure doesn't declare any foreach types "+value["foreach"])
+            return foreach_map[value["foreach"]][value["name"]]
+        else:
+            return None
+
+    def expand_foreach(self, foreach, env):
+        all_foreach = []
+
+        for f in foreach:
+            flist = env.lookup(f)
+            if flist is None:
+                print("Warning: No items named " + f + " available for foreach")
+                continue
+
+            if len(all_foreach) == 0:
+                for fitem in flist:
+                    all_foreach.append({f: fitem})
+            else:
+                new_all = []
+                for fitem in flist:
+                    for allitem in all_foreach:
+                        c = allitem.copy()
+                        c[f] = fitem
+                        new_all.append(c)
+                all_foreach = new_all
+        return all_foreach
+
+    def render(self, parent, obj):
+        return self.render_with_env(parent, obj, Environment(obj))
+
+    def render_with_env(self, parent, obj, env):
+        if obj["struct_type"] not in self.schemas:
+            raise RenderError("No schema definition for type "+obj["struct_type"])
+
+        s1 = self.schemas[obj["struct_type"]]
+
+        if obj["type"] in s1:
+            schema = s1[obj["type"]]
+        elif "" in s1:
+            schema = s1[""]
+        else:
+            raise RenderError("No schema definition for type "+obj["struct_type"] +
+                              " subtype "+obj["type"])
+
+
+        if "xml" not in schema:
+            raise RenderError("No XML description for type "+obj["struct_type"] +
+                              " subtype "+obj["type"])
+
+        xml_schema = schema["xml"]
+
+        return self.render_with_schema(parent, obj, env, xml_schema, {})
+
+    def render_with_schema(self, parent, obj, env, xml_schema, foreach_map):
+        if type(obj) is dict:
+            if "variable_definitions" in obj:
+                variable_environments = [[]]
+                for variable_definition in obj["variable_definitions"]:
+                    referenced = variable_definition["reference"]
+                    new_ve = []
+                    if type(referenced) is list:
+                        for ve in variable_environments:
+                            for item in referenced:
+                                ve2 = ve[:]
+                                ve2.append([variable_definition["variable"], item])
+                                new_ve.append(ve2)
+                    else:
+                        for ve in variable_environments:
+                            ve2 = ve[:]
+                            ve2.append([variable_definition["variable"], referenced])
+                            new_ve.append(ve2)
+
+                    variable_environments = new_ve
+
+                    for ve in variable_environments:
+                        new_dict = {}
+                        for vr in ve:
+                            new_dict[vr[0]] = vr[1]
+                        new_env = Environment(new_dict, env)
+                        self.render_with_schema_impl(parent, obj, new_env, xml_schema, foreach_map)
+            else:
+                return self.render_with_schema_impl(parent, obj, env, xml_schema, foreach_map)
+        else:
+            return self.render_with_schema_impl(parent, obj, env, xml_schema, foreach_map)
+
+    def render_with_schema_impl(self, parent, obj, env, xml_schema, foreach_map):
+        tag_name = ""
+
+        if len(xml_schema) == 1 and xml_schema[0]["type"] == "foreach":
+            param = xml_schema[0]
+            foreach_list = self.expand_foreach(param["foreach"], env)
+            first_elem = None
+            for foreach_map in foreach_list:
+                elem = self.render_with_schema(parent, obj, env, param["foreach_schema"], foreach_map)
+                if first_elem is None:
+                    first_elem = elem
+            return first_elem
+
+        for param in xml_schema:
+            if param["type"] == "tag":
+                tag_name = self.get_value(obj, param["tag_value"], env, foreach_map)
+                break
+            elif param["type"] == "render" and not "containing_tag" in param:
+                return self.render_with_env(parent, obj, env)
+
+        if tag_name == "":
+            raise RenderError("No XML tag name defined for type "+obj["struct_type"] +
+                              " subtype "+obj["type"])
+
+        if parent is None:
+            element = ET.Element(tag_name)
+        else:
+            element = ET.SubElement(parent, tag_name)
+
+        for param in xml_schema:
+            if param["type"] == "attr":
+                element.attrib[param["attr_type"]] = str(self.get_value(obj, param["attr_value"], env, foreach_map))
+            elif param["type"] == "children":
+                children = []
+                if param["children_source"] in obj:
+                    children = obj[param["children_source"]]
+                child_container = element
+                if "containing_tag" in param and param["containing_tag"] is not None:
+                    child_container = ET.SubElement(element, param["containing_tag"])
+                for child in children:
+                    self.render_with_schema(child_container, child, env, param["children_schema"], foreach_map)
+            elif param["type"] == "child":
+                child_elem = ET.SubElement(element, param["tag_name"])
+                child_elem.text = str(self.get_value(obj, param["child_value"], env, foreach_map))
+            elif param["type"] == "reference":
+                collection = env.lookup(param["collection"])
+                if collection is None:
+                    raise RenderError("No collection of items named "+param["collection"])
+                for item in collection:
+                    if item["type"] == param["name"]:
+                        if param["param"] not in item:
+                            raise RenderError("Can't find "+param["param"]+" in item "+param["name"]+" in "+param["collection"])
+                    element.text = str(item[param["name"]])
+            elif param["type"] == "foreach":
+                foreach_list = self.expand_foreach(param["foreach"], env)
+                for foreach_map in foreach_list:
+                    self.render_with_schema(element, obj, env, param["foreach_schema"], foreach_map)
+            elif param["type"] == "value":
+                element.text = str(self.get_value(obj, param["value"], env, foreach_map))
+            elif param["type"] == "render":
+                child_container = element
+                if "containing_tag" in param and param["containing_tag"] is not None:
+                    child_container = ET.SubElement(element, param["containing_tag"])
+                object_to_render = self.get_value(obj, param["value"], env, foreach_map)
+                self.render_with_env(child_container, object_to_render, env)
+
+        return element
